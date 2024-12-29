ed28c26acad2ea232765cdc585edaee700048423
Mikko Lehtisalo <mikkolehtisalo@gmail.com>
2024-12-29 14:51:46 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # osdump
-A tool for dumping opensearch index to a file
+
+`osdump` is a high performance tool for extracting documents from OpenSearch indexes and saving them into files. 
+
+## Features
+
+* As high performance as a single worker solution can be
+* Opensearch queries are based on `search_after`
+* Uses `fastjson` for faster json parsing
+* Built-in support for compressing the output using `brotli`
+* Has some built-in sanity checks to ensure smooth operation
+
+## Installation
+
+```bash
+go install github.com/mikkolehtisalo/osdump@latest
+```
+
+## Usage
+
+The configuration options:
+```bash
+$ ./osdump -h
+Usage of ./osdump:
+  -base string
+        opensearch base url (default "https://localhost:9200")
+  -brotli
+        compress using brotli
+  -ca string
+        CA certificate (default "ca.pem")
+  -file string
+        target file for export (default "graylog_0.json")
+  -index string
+        opensearch index (default "graylog_0")
+  -loglevel string
+        level of logging (default "info")
+  -password string
+        opensearch user (default "password")
+  -quality int
+        brotli quality setting (default 4)
+  -size int
+        search window size (default 1000)
+  -user string
+        opensearch user (default "graylog")
+```
+
+Example run:
+```bash
+$ ./osdump -user admin -password mysecretpassword -size 1000
+2024/12/29 14:26:11 INFO Starting to dump graylog_0
+2024/12/29 14:26:11 INFO Index graylog_0 has 272905 documents to dump
+2024/12/29 14:27:37 INFO Dumped 272905 records in 86 seconds, average speed 3159/second
+2024/12/29 14:27:37 INFO Finished dumping graylog_0
+```
+
+## Requirements
+
+* Go 1.22+
+* Access to an OpenSearch instance
+
+## Limitations
+
+* Large dumps may require large amounts of disk space
+* Brotli's performance for compression is abysmal
+* Assumes opensearch security is configured (TLS enabled, and username/password required)
+* Single worker for querying opensearch, for now
+
+## Contributing
+
+This works for me. If you need more features, or find a bug, please open a pr, or an issue.
+
+## License
+
+osdump is licensed under the MIT License.
+
---
+++ osdump.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"text/template"
+	"time"
+
+	"github.com/charmbracelet/log"
+	cbrotli "github.com/google/brotli/go/cbrotli"
+	"github.com/valyala/fastjson"
+)
+
+// Holds the configuration
+type Configuration struct {
+	Base      string
+	User      string
+	Password  string
+	Tls       bool
+	Tls_ca    string
+	Index     string
+	Size      int
+	File      string
+	Brotli    bool
+	Quality   int
+	Log_level string
+}
+
+// Holds the dump context
+type Context struct {
+	Size     int
+	After    string
+	Counter  int
+	Client   *http.Client
+	Parser   *fastjson.Parser
+	Template *template.Template
+	Tasks    *chan []byte
+}
+
+// Line feed "constant"
+var ln = []byte{10}
+
+// Query template for search_after
+const query_template string = `{
+	"size": {{.Size}},{{if .After}}
+	"search_after": ["{{.After}}"],{{end}}
+	"sort": [
+	  { "_id": "asc" } 
+	]
+}`
+
+// Helper for checking errs
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+// Helper for debug logging
+func debugf(format string, args ...interface{}) {
+	// Without this kind of check the sprintf will get called even though the final message might be discarded
+	if log.GetLevel() == log.DebugLevel {
+		debugf(format, args...)
+	}
+}
+
+// Gets configuration from the command line parameters
+func get_config() *Configuration {
+	var config Configuration
+	flag.StringVar(&config.Base, "base", "https://localhost:9200", "opensearch base url")
+	flag.StringVar(&config.User, "user", "graylog", "opensearch user")
+	flag.StringVar(&config.Password, "password", "password", "opensearch user")
+	flag.StringVar(&config.Tls_ca, "ca", "ca.pem", "CA certificate")
+	flag.StringVar(&config.Index, "index", "graylog_0", "opensearch index")
+	flag.IntVar(&config.Size, "size", 1000, "search window size")
+	flag.StringVar(&config.File, "file", "graylog_0.json", "target file for export")
+	flag.BoolVar(&config.Brotli, "brotli", false, "compress using brotli")
+	flag.IntVar(&config.Quality, "quality", 4, "brotli quality setting")
+	flag.StringVar(&config.Log_level, "loglevel", "info", "level of logging")
+	flag.Parse()
+	if strings.HasPrefix(config.Base, "https") {
+		config.Tls = true
+	}
+	level, err := log.ParseLevel(config.Log_level)
+	if err == nil {
+		log.SetLevel(level)
+	}
+	debugf("Configuration: %+v", config)
+	return &config
+}
+
+// Builds opensearch query template
+func build_query_template() *template.Template {
+	tmpl, err := template.New("query").Parse(query_template)
+	check(err)
+	debugf("Query template: %+v", tmpl)
+	return tmpl
+}
+
+// Builds HTTP or HTTPS client depending on configuration
+func build_http_client(conf *Configuration) *http.Client {
+	// If configured, build TLS client
+	if conf.Tls {
+		return build_tls_http_client(conf)
+	}
+	// Else build non-TLS client
+	debugf("Built http client")
+	return &http.Client{}
+}
+
+// Builds HTTPS client, if requested
+func build_tls_http_client(conf *Configuration) *http.Client {
+	tlsConfig := &tls.Config{RootCAs: x509.NewCertPool()}
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client := &http.Client{Transport: transport}
+	pemData, err := os.ReadFile(conf.Tls_ca)
+	check(err)
+	ok := tlsConfig.RootCAs.AppendCertsFromPEM(pemData)
+	if !ok {
+		log.Fatalf("Parsing CA certificate failed!")
+	}
+	debugf("Built https client")
+	return client
+}
+
+// Helper function for opensearch queries
+func http_get(uri string, body []byte, config *Configuration, ctx *Context) []byte {
+	debugf("URI for HTTP GET: %s", uri)
+	br := bytes.NewReader(body)
+	req, err := http.NewRequest("GET", uri, br)
+	check(err)
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(config.User, config.Password)
+	resp, err := ctx.Client.Do(req)
+	check(err)
+	defer resp.Body.Close()
+	debugf("Response code: %d", resp.StatusCode)
+	bodyBytes, err := io.ReadAll(resp.Body)
+	check(err)
+	debugf("Response body: %s", bodyBytes)
+	// Anything besides 200 OK is probably fatal
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Got invalid HTTP status code: %d", resp.StatusCode)
+	}
+	return bodyBytes
+}
+
+// Queries the opensearch for total amount of data
+func query_count_database(config *Configuration, ctx *Context) int {
+	count := 0
+	// Request
+	uri := fmt.Sprintf("%s/%s/_count", config.Base, config.Index)
+	body := http_get(uri, nil, config, ctx)
+	// Handle results
+	json, err := ctx.Parser.ParseBytes(body)
+	check(err)
+	if json.Exists("count") {
+		count = json.GetInt("count")
+	}
+	debugf("Returning count %d", count)
+	return count
+}
+
+// Queries the opensearch for one window of data
+func query_search_database(config *Configuration, ctx *Context) []byte {
+	uri := fmt.Sprintf("%s/%s/_search", config.Base, config.Index)
+	buf := new(bytes.Buffer)
+	err := ctx.Template.Execute(buf, ctx)
+	check(err)
+	bodyBytes := http_get(uri, buf.Bytes(), config, ctx)
+	return bodyBytes
+}
+
+// Parse search results for single window
+func parse_search_results(input []byte, ctx *Context) [][]byte {
+	var result [][]byte
+	// Parse JSON
+	json, err := ctx.Parser.ParseBytes(input)
+	check(err)
+	// Sanity check
+	if !json.Exists("hits") {
+		log.Fatalf("JSON result looks incorrect: %s", json)
+	}
+	results := json.Get("hits").Get("hits").GetArray()
+	// If the array is empty, we probably just parsed everything already
+	if len(results) == 0 {
+		log.Debug("Did not get any results, bailing out")
+		return [][]byte{}
+	}
+
+	// Iterate over results
+	for _, v := range results {
+		// Update the search_after
+		sort := string(v.GetStringBytes("sort", "0"))
+		if sort != "" {
+			ctx.After = sort
+		}
+		// Remove sort information
+		if v.Exists("sort") {
+			v.Del("sort")
+		}
+		// Increase query counter
+		ctx.Counter++
+		// Add to results
+		result = append(result, v.MarshalTo([]byte{}))
+	}
+	return result
+}
+
+// Loops the search and sends the results to a channel
+func producer(ctx *Context, config *Configuration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		q := query_search_database(config, ctx)
+		r := parse_search_results(q, ctx)
+		for x := range r {
+			*ctx.Tasks <- r[x]
+		}
+		if len(r) == 0 {
+			log.Debug("Nothing more to produce, breaking the loop")
+			break
+		}
+	}
+	log.Debug("Producer done")
+}
+
+// Reads results from a channel and writes them
+func consumer(ctx *Context, config *Configuration, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	// Prepare the output file for writing
+	// Use os.O_CREATE and os.O_EXCL flags to ensure the file is created only if it does not already exist
+	f, err := os.OpenFile(config.File, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
+	check(err)
+	w := bufio.NewWriter(f)
+
+	// Build a writer that works both with straight buffering, and brotli's writer
+	// Apparently only io.Writer seems to be common with these two writers
+	var out io.Writer
+	if config.Brotli {
+		opts := cbrotli.WriterOptions{}
+		opts.Quality = config.Quality
+		cout := cbrotli.NewWriter(w, opts)
+		out = cout
+		defer func() {
+			cout.Flush()
+			cout.Close()
+			w.Flush()
+			f.Close()
+		}()
+	} else {
+		out = w
+		defer func() {
+			w.Flush()
+			f.Close()
+		}()
+	}
+
+	// Write received data
+	for data := range *ctx.Tasks {
+		out.Write(data)
+		out.Write(ln) // \n
+	}
+	log.Debug("Consumer done")
+}
+
+func main() {
+	config := get_config()
+	var ctx Context
+	ctx.Size = config.Size
+	ctx.Template = build_query_template()
+	ctx.Client = build_http_client(config)
+	ctx.Parser = &fastjson.Parser{}
+	tasksChan := make(chan []byte, 100000)
+	ctx.Tasks = &tasksChan
+
+	log.Infof("Starting to dump %s", config.Index)
+	start := time.Now()
+	// Check the count of documents
+	c := query_count_database(config, &ctx)
+	log.Infof("Index %s has %d documents to dump", config.Index, c)
+	if c == 0 {
+		log.Fatal("Nothing to dump!")
+	}
+	// Set up producer
+	var pwg sync.WaitGroup
+	pwg.Add(1)
+	go producer(&ctx, config, &pwg)
+	go func() {
+		pwg.Wait()
+		close(*ctx.Tasks)
+		log.Debug("Closed tasks channel")
+	}()
+	// Set up consumer
+	var cwg sync.WaitGroup
+	cwg.Add(1)
+	go consumer(&ctx, config, &cwg)
+	cwg.Wait()
+	// Print statistics
+	elapsed := time.Since(start)
+	log.Infof("Dumped %d records in %d seconds, average speed %d/second", ctx.Counter, int(elapsed.Seconds()), int(float64(ctx.Counter)/elapsed.Seconds()))
+	log.Infof("Finished dumping %s", config.Index)
+}
